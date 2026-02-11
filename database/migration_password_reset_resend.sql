e5230262eed131c3579cd8b297d1054817138231
Seif Medhat <seifmedhat.sm@gmail.com>
2026-02-11 07:54:14 +0200

    clean files

--- database/migration_password_reset_resend.sql
+++
@@ -1,3 +0,0 @@
--- Add resend tracking to password_resets for rate-limited resend feature
-ALTER TABLE password_resets ADD COLUMN IF NOT EXISTS resend_count INTEGER DEFAULT 0;
-ALTER TABLE password_resets ADD COLUMN IF NOT EXISTS last_sent_at TIMESTAMP DEFAULT NOW();
